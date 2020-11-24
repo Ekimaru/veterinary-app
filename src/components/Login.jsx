bb53db859ba14b20374e59c0c34de5966a8a7a65
Michael <michaelporshe64@gmail.com>
2020-11-23 23:01:17 -0500

    commit pantalla login api google y facebook

---
+++ src/components/Login.jsx
@@ -0,0 +1,88 @@
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import Button from '@material-ui/core/Button';
+import FacebookLogin from 'react-facebook-login';
+import GoogleLogin from 'react-google-login';
+
+const useStyles = makeStyles((theme) => ({
+    root: {
+        height: '100vh',
+        fontFamily: 'Nunito',
+        
+        
+
+        
+    },
+    title: {
+        fontSize: '3rem',
+        fontFamily: 'Nunito',
+        color: '#fff',
+        justifyContent: 'center',
+        TextAlign: 'center',
+        alignItems: 'top',
+        display: 'flex',
+    },
+    content: {
+        
+        justifyContent: 'center',        
+        alignItems: 'center',
+        alignItems: 'center',
+        fontFamily: 'Nunito',
+        TextAlign: 'center',
+        display: 'flex',
+        margin: '10%',
+       
+    },
+    content2: {
+        alignItems: 'center',
+        fontFamily: 'Nunito',
+        margin: '13%',
+        background: 'none',
+        width: '50',
+
+    },
+
+        
+
+
+    }
+));
+export default function () {
+    const classes= useStyles();
+    const responseFacebook = (response) => {
+        console.log(response);
+
+    }
+    const responseGoogle = (response) => {
+            console.log(response);
+    }
+
+    return <div>
+             <div className={classes.root} id="login">
+             <h1 className={classes.title}>Login</h1>;
+             <div>
+                 <div className={classes.content}><FacebookLogin
+                     appId="823378155159729"
+                     fields="name,email,picture"
+                     callback={responseFacebook} />
+                 </div>
+
+                 <div className={classes.content}> <GoogleLogin
+                     clientId="737999626252-fr4jekmervv7uin4eopv2rvcqunp0fl9.apps.googleusercontent.com"
+                     buttonText="LOGIN WITH GOOGLE"
+                     onSuccess={responseGoogle}
+                     onFailure={responseGoogle}
+                     cookiePolicy={'single_host_origin'}/> 
+                     </div>
+
+
+             
+             </div>
+             </div>
+             
+           </div>
+       
+           
+    
+    
+}
